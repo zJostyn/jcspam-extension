4ad1375cf1bdcb9ddaab8e6f43ab6e5a9c748967
zjostyn <jostyncruzquito@gmail.com>
2024-12-24 12:12:49 -0500

    Extension Para Responder Formularios

---
+++ README.md
@@ -0,0 +1,27 @@
+# Borang Chrome Extension | Google Form spammer
+
+## ⚠ Open-source version of Borang is archived
+
+- The existing source code will remain on Github
+
+- Borang will be developed privately
+
+- We hope to meet again with other cool open-source projects!"
+
+
+## About this project
+
+- This open-source project was initially created for students who need to get certain amount of respondents for their survey.
+
+- This repo is for Borang's chrome extension.
+
+- Web App: [borang.skrin.xyz](https://borang.skrin.xyz)
+
+- You can get the source code here: 
+[Borang web app repo](https://github.com/adibzter/borang)
+
+- Install from Chrome Web Store: 
+[Borang Chrome Extension install](https://chrome.google.com/webstore/detail/borang/mokcmggiibmlpblkcdnblmajnplennol)
+
+## Licence
+This project is licensed under the [MIT License](https://github.com/ADIBzTER/borang-chrome-extension/blob/master/LICENSE)
---
+++ content/content.js
@@ -0,0 +1,82 @@
+//const submitUrl = 'https://borang.skrin.xyz/';
+const submitUrl = 'http://localhost:5000/submit';
+
+chrome.storage.local.get(['isBorangEnabled']).then((result) => {
+  injectDiv(result.isBorangEnabled);
+
+  if (result.isBorangEnabled) {
+    enableBorang();
+  }
+});
+
+function injectDiv(isBorangEnabled) {
+  if (location.pathname.startsWith('/_submit')) {
+    return;
+  }
+
+  const div = document.createElement('div');
+  div.id = 'borang-div';
+  div.textContent = isBorangEnabled ? 'Borang Enabled' : 'Borang Disabled';
+
+  const input = document.createElement('input');
+  input.type = 'checkbox';
+  input.checked = isBorangEnabled;
+  input.onchange = async (e) => {
+    await chrome.storage.local.set({ isBorangEnabled: e.target.checked });
+    location.reload();
+  };
+
+  document.documentElement.append(div);
+  div.prepend(input);
+}
+
+function enableBorang() {
+  let submitButton = document.querySelector(
+    // these used to be the selectors for the submit button, keep here for reference
+    // maybe what can be done is to query button element with text 'Submit'
+    // '.freebirdFormviewerViewNavigationSubmitButton'
+    // '.uArJ5e.UQuaGc.Y5sE8d.VkkpIf.NqnGTe'
+    '.uArJ5e.UQuaGc.Y5sE8d.VkkpIf'
+  );
+
+  if (submitButton) {
+    const form = document.querySelector('form');
+
+    submitButton.onclick = () => {
+      const formUrl = form.action;
+      form.method = 'POST';
+      form.action = submitUrl;
+
+      const counter = +prompt('How many times should the form be submitted?');
+      if (!counter) {
+        alert('Please enter a number');
+        return;
+      }
+      submitForm(formUrl, counter);
+    };
+  }
+}
+
+function submitForm(formUrl, submitNumber) {
+  const url = document.createElement('input');
+  url.type = 'hidden';
+  url.setAttribute('name', 'url');
+  url.value = formUrl;
+
+  const form = document.querySelector('form');
+  form.appendChild(url);
+
+  const counter = document.createElement('input');
+  counter.type = 'hidden';
+  counter.setAttribute('name', 'counter');
+  counter.value = submitNumber;
+  form.appendChild(counter);
+
+  const fromExtension = document.createElement('input');
+  fromExtension.type = 'hidden';
+  fromExtension.setAttribute('name', 'fromExtension');
+  fromExtension.value = true;
+  form.appendChild(fromExtension);
+
+  form.submit();
+}
