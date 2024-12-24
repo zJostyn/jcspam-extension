1523d231e21d1bd5a95a32b379b2d36bf1077772
zjostyn <jostyncruzquito@gmail.com>
2024-12-24 13:47:13 -0500

    Modificacion en el diseño y funcionalidad al activar la extension

--- content/content.js
+++ content/content.js
@@ -1,36 +1,54 @@
 //const submitUrl = 'https://jcspam.vercel.app/submit';
 const submitUrl = 'http://localhost:5000/submit';
 
-chrome.storage.local.get(['isBorangEnabled']).then((result) => {
-  injectDiv(result.isBorangEnabled);
+chrome.storage.local.get(['isJCSpamEnabled']).then((result) => {
+  injectDiv(result.isJCSpamEnabled);
 
-  if (result.isBorangEnabled) {
-    enableBorang();
+  if (result.isJCSpamEnabled) {
+    enableJCSpam();
   }
 });
 
-function injectDiv(isBorangEnabled) {
+function injectDiv(isJCSpamEnabled) {
   if (location.pathname.startsWith('/_submit')) {
     return;
   }
 
   const div = document.createElement('div');
-  div.id = 'borang-div';
-  div.textContent = isBorangEnabled ? 'Borang Enabled' : 'Borang Disabled';
-
+  div.id = 'jcspam-div';
+  div.textContent = isJCSpamEnabled ? 'JCSpam Activado' : 'JCSpam Desactivado';
+  div.style.width = '220px';
   const input = document.createElement('input');
   input.type = 'checkbox';
-  input.checked = isBorangEnabled;
+  input.checked = isJCSpamEnabled;
+  input.style.display = 'none';
   input.onchange = async (e) => {
-    await chrome.storage.local.set({ isBorangEnabled: e.target.checked });
+    await chrome.storage.local.set({ isJCSpamEnabled: e.target.checked });
     location.reload();
   };
 
+  const img = document.createElement('img');
+  img.src = chrome.runtime.getURL('icon_128.png');
+  img.style.position = 'relative';
+  img.style.cursor = 'pointer';
+  img.style.left = input.checked ? '-2px' : '10px';
+  img.style.width = '50px';
+  img.style.height = '50px';
+  img.style.borderRadius = '5px';
+  img.onclick = async () => {
+    input.checked = !input.checked;
+    await chrome.storage.local.set({ isJCSpamEnabled: input.checked });
+    div.textContent = input.checked ? 'JCSpam Activado' : 'JCSpam Desactivado';
+    img.style.left = input.checked ? '-2px' : '10px';
+    div.prepend(img);
+    location.reload();
+  }
   document.documentElement.append(div);
   div.prepend(input);
+  div.prepend(img);
 }
 
-function enableBorang() {
+function enableJCSpam() {
   let submitButton = document.querySelector(
     // these used to be the selectors for the submit button, keep here for reference
     // maybe what can be done is to query button element with text 'Submit'
@@ -47,9 +65,9 @@ function enableBorang() {
       form.method = 'POST';
       form.action = submitUrl;
 
-      const counter = +prompt('How many times should the form be submitted?');
+      const counter = +prompt('Cuantas veces quieres que se envie el formulario?');
       if (!counter) {
-        alert('Please enter a number');
+        alert('Ingrese la cantidad deseada');
         return;
       }
       submitForm(formUrl, counter);
